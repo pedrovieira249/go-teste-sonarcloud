4fdeb9df721a567c1f3d492c031b68d35e7d0c2d
Pedro Vieira <angus.pedro@hotmail.com>
2025-11-21 08:58:57 -0300

    ci: :green_heart: Melhorando cobertura de codigo

--- sum.go
+++ sum.go
@@ -1,26 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println(soma(32, 32))
-	fmt.Println(sub(512, 256))
-	fmt.Println(multi(32, 16))
-	fmt.Println(divisao(1024, 256))
+	runCalculations()
 }
 
-func soma(a int, b int) int {
+// runCalculations executa todas as operações matemáticas
+// Esta função pode ser testada facilmente
+func runCalculations() {
+	fmt.Println("Soma:", soma(32, 32))
+	fmt.Println("Subtração:", sub(512, 256))
+	fmt.Println("Multiplicação:", multi(32, 16))
+
+	result, err := divisao(1024, 256)
+	if err != nil {
+		fmt.Println("Erro na divisão:", err)
+	} else {
+		fmt.Println("Divisão:", result)
+	}
+
+	_, err = divisao(10, 0)
+	if err != nil {
+		fmt.Println("Erro esperado:", err)
+	}
+}
+
+// soma retorna a soma de dois números inteiros
+func soma(a, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
+// sub retorna a subtração de dois números inteiros
+func sub(a, b int) int {
 	return a - b
 }
 
-func multi(a int, b int) int {
+// multi retorna a multiplicação de dois números inteiros
+func multi(a, b int) int {
 	return a * b
 }
 
-func divisao(a int, b int) int {
-	return a / b
+// divisao retorna a divisão de dois números inteiros
+// Retorna erro se o divisor for zero
+func divisao(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("divisão por zero não é permitida")
+	}
+	return a / b, nil
 }
